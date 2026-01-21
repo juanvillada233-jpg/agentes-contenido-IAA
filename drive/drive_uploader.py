1d79bb81947c708f6f4591e26ba1f68c53fcd93a
juanvillada233-jpg <juan.villada233@gmail.com>
2026-01-20 21:15:45 -0500

    Update drive_uploader.py

--- drive/drive_uploader.py
+++ drive/drive_uploader.py
@@ -1,34 +1,47 @@
-from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import pickle
 import os
 
-SCOPES = ['https://www.googleapis.com/auth/drive.file']
+SCOPES = ["https://www.googleapis.com/auth/drive.file"]
 
 def subir_a_drive(ruta_archivo, nombre_archivo):
-    creds_info = os.getenv("GOOGLE_SERVICE_ACCOUNT")
+    creds = None
 
-    credentials = service_account.Credentials.from_service_account_info(
-        eval(creds_info),
-        scopes=SCOPES
-    )
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
 
-    service = build('drive', 'v3', credentials=credentials)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    service = build("drive", "v3", credentials=creds)
 
     file_metadata = {
-        'name': nombre_archivo,
-        'parents': [os.getenv("DRIVE_FOLDER_ID")]
+        "name": nombre_archivo,
+        "parents": [os.getenv("DRIVE_FOLDER_ID")]
     }
 
     media = MediaFileUpload(
         ruta_archivo,
-        mimetype='application/json'
+        mimetype="application/json"
     )
 
     file = service.files().create(
         body=file_metadata,
         media_body=media,
-        fields='id'
+        fields="id"
     ).execute()
 
-    return file.get('id')
+    print("Archivo subido a Drive con ID:", file.get("id"))
