032860d7d78dce8bea388a2d8917c97231a45bb7
juanvillada233-jpg <juan.villada233@gmail.com>
2026-03-20 19:31:59 -0500

    Delete .github/main.py

--- .github/main.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-print("--- 🤖 PROBANDO UBICACIÓN CORRECTA ---", flush=True)
-
-if __name__ == "__main__":
-    print("✅ ¡ÉXITO! Ahora sí estoy en el lugar correcto.", flush=True)
-    print(f"Carpeta actual: {os.getcwd()}", flush=True)
-    print(f"Archivos aquí: {os.listdir('.')}", flush=True)
